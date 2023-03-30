3b3805c7374cdac7982749fec8a8644347eab8c4
Roberto Flores <rob.floresm04@gmail.com>
2023-03-29 23:53:09 -0600

    Proyecto 2

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('orders', OrdersController::class);
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
@@ -28,4 +26,5 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+    Route::resource('orders', OrdersController::class);
 });
